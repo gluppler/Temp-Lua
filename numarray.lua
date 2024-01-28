6defd242e701475f33a7ac9805aef16a8a09b8e7
gluppler <chewzhanhongint@gmail.com>
2024-01-28 14:42:25 +0800

    Unfinished

---
+++ numarray.lua
@@ -0,0 +1,26 @@
+-- Var --
+local nums = {}
+local size
+local count = 0
+local num
+
+-- Rewriting some of my java programs into Lua
+-- Script --
+print("Define number size: ")
+size = io.read("n")
+
+nums = nums[size]
+
+while count < #nums  do
+
+    print("Enter a number: ")
+    num = io.read("n")
+
+    if num >= 10 and num <= 100 then
+
+        local hasNum = false
+
+        for i = 0 --Work in Progress
+    end
+end
+
